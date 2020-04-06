fd74461fe41640df7572ce947694ca5377998633
Alexandre Miyazaki <alexandre.miyazaki@corp.globo.com>
2020-04-06 07:48:53 -0300

    code testing lazy val

---
+++ LazyVal/build.sbt
@@ -0,0 +1,5 @@
+name := "LazyVal"
+
+version := "0.1"
+
+scalaVersion := "2.11.12"
---
+++ LazyVal/src/main/scala/LazyVal.scala
@@ -0,0 +1,17 @@
+
+object LazyVal extends App {
+  lazy val iHaveAnExpensiveInitialization = Initialize()
+
+  def Initialize():String = {
+    println("Initializing...")
+    Thread.sleep(1000 * 10)
+    println("Initialization completed")
+    "Initialized!"
+  }
+
+  println("Started")
+  println(iHaveAnExpensiveInitialization)
+  println(iHaveAnExpensiveInitialization)
+  println(iHaveAnExpensiveInitialization)
+  println("Ended")
+}
